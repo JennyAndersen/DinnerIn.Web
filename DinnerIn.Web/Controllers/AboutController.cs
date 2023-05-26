ccd875c8eccc9834b1d117f118bdcb6f1b833cb3
JennyAndersen <jenny.andersen@student.nbi-handelsakademin.se>
2023-05-26 11:15:22 +0200

    About us

---
+++ DinnerIn.Web/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DinnerIn.Web.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
